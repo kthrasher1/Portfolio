ba1e533e21323fb8a2896c0435fecf7bf25593f4
Kurtis Thrasher <kthrasher1@hotmail.co.uk>
2018-04-14 21:32:59 +0100

    Main files for porfolio
    - Added main files for the website
    - Added Gulp
    - Added Node
    - Added Bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/styles/*.sass'])
+        .pipe(sass())
+        .pipe(gulp.dest("public"))
+        .pipe(browserSync.stream());
+});
+
+// Move the javascript files into our /src/js folder
+gulp.task('js', function() {
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 
+    'node_modules/popper.js/dist/umd/popper.min.js'])
+        .pipe(gulp.dest("src/scripts"))
+        .pipe(browserSync.stream());
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: {
+           baseDir: "/build"
+        }
+       
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/styles/*.sass'], ['sass']);
+    gulp.watch("build/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['js','serve']);
