bd9bef65a492483719d0010f706c1550bdf42343
Kurtis <kthrasher1@hotmail.co.uk>
2018-05-20 22:45:03 +0100

    A FUCKING LOT

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ gulp.task('sass', function() {
 gulp.task('js', function() {
     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 
     'node_modules/popper.js/dist/umd/popper.min.js'])
-        .pipe(gulp.dest("src/scripts"))
+        .pipe(gulp.dest("build/scripts"))
         .pipe(browserSync.stream());
 });
 
