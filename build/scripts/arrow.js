2f34322b477a85ebea941dee3f6ba9355aec50b9
Kurtis <kthrasher1@hotmail.co.uk>
2018-08-20 17:15:21 +0100

    Changes advised by Matt Ellis

--- build/scripts/arrow.js
+++ build/scripts/arrow.js
@@ -21,5 +21,24 @@ jQuery(document).ready(function($) {
             scrollTop: 0
         }, 'slow');
     });
+    
+
+    
+    
+    
+});
+
+jQuery(document).ready(function($) {
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function () {
+        $('a.page-scroll').bind('click', function (event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1550, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    });
+
 
 });
